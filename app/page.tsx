c9355b7ad788f4c0d1c64e6a008d674ce7c42acf
yoak__ <ritik.raj@webskitters.com>
2024-10-15 00:58:30 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
+      <span className="mb-10 font-semibold text-xl text-gray-500" > console.log(<span> have a good day : </span>)</span>
+    </main>
+  );
+}
---
+++ components/about.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        Proficient in crafting robust, scalable solutions. I am dedicated to contribute my skillset to foster a
+        collaborative and dynamic work environment, consistently striving for the advancement of team goals, I am
+        committed to continuous improvement, investing time and effort into refining my skills to deliver impactful
+        results.
+      </p>
+    </motion.section>
+  );
+}
